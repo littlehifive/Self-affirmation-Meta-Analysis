b85f27abfb762211132f5db5a89f35c17656a44c
Michael Wu <wuzezhen33@gmail.com>
2020-07-31 15:39:36 -0400

    Add comments

--- R/functions.R
+++ R/functions.R
@@ -1,8 +1,11 @@
+# A list of functions used in calculations:
 
+# Extract useful information about g from esc functions outputs
 extract_g <- function(x){
   return(as.numeric(unlist(x)[c(1,3,4,5)]))
 }
 
+# Reported effect size and variance to Hedges' g
 dv2g <- function(d,v_d,n){
   J= 1- 3/ (4*n-9)
   g = J*d
@@ -12,6 +15,7 @@ dv2g <- function(d,v_d,n){
   return(c(g, v_g, lowerCI, upperCI))
 }
 
+# Cohen's d to Hedges'g
 d2g <- function(d, nt, nc){
   
   g = des(d, nt, nc, verbose = F, dig = 5)[1,12:15]
@@ -19,6 +23,7 @@ d2g <- function(d, nt, nc){
   
 }
 
+#  F statistic to Hedges'g
 f2g <- function(d, nt, nc){
   
   g = fes(d, nt, nc, verbose = F, dig = 5)[1,12:15]
@@ -28,6 +33,9 @@ f2g <- function(d, nt, nc){
 
 # Peterson & Brown, 2005, <On the Use of Beta Coefficients in Meta-Analysis>
 # This is for standardized beta only
+# This function is unused in the current meta-analysis, 
+# but could be useful for future analysis if one only reports standardized beta but nothing else
+
 B2d <- function(B,n){
   # B to r
   lamda = sign(B)
