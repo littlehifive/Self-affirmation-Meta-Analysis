7d37bb02dabd79b06d4ba5d66ffaa93470e217aa
Michael Wu <wuzezhen33@gmail.com>
2020-08-10 09:50:21 -0400

    Update packages.R

--- R/packages.R
+++ R/packages.R
@@ -4,4 +4,3 @@ library(metafor) # package for conducting meta-analysis
 library(compute.es) # package for calculating effect sizes
 library(esc) # package for calculating EF from regression model
 library(emmeans) # package for calculating adjusted means
-library(poibin)
