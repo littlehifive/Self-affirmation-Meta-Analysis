18383c235503d2dfb847cb7a3d15549e03498628
Michael Wu <wuzezhen33@gmail.com>
2020-09-11 21:04:14 -0400

    update models

--- R/additional models.R
+++ R/additional models.R
@@ -39,21 +39,38 @@ model.mods<-rma.mv(es,
 model.mods
 
 
+data_all_outcomes_minority <- data_all_outcomes_minority %>%
+  mutate(before_stress = if_else(timing == "before_stress", 1, 0),
+         both = if_else(timing == "both", 1, 0),
+         early = if_else(timing == "early", 1, 0),
+         neither = if_else(timing == "neither", 1, 0)
+         )
+
+data_all_outcomes_minority$timing <- factor(data_all_outcomes_minority$timing,
+                                            levels = c( "early", "neither","before_stress","both"))
 model.mods<-rma.mv(es, 
                     v, 
                     random = list( 
-                          ~ 1 | cluster/study), 
+                          ~ 1 | cluster), 
                     tdist = TRUE, 
                     data = data_all_outcomes_minority,
                     method = "REML",
+                   mods = ~ early + before_stress)
+model.mods
+
+model.mods<-rma.mv(es, 
+                   v, 
+                   random = list( 
+                     ~ 1 | cluster/study), 
+                   tdist = TRUE, 
+                   data = data_all_outcomes_minority,
+                   method = "REML",
                    mods = ~ duration_rough_month + ordinary + adapted + timing + delivered_in_classroom + density_freelunch)
 model.mods
 
-
 # exploratory analysis
-dat$fidelity_implementation <- rowSums(dat[,c("timing_z","ordinary_z", 
-                                              "duration_rough_month_z", "adapted_z",
-                                              "delivered_in_classroom_z", "control_type_z")], na.rm = T)
+dat$fidelity_implementation <- rowSums(dat[,c("ordinary_z", 
+                                              "duration_rough_month_z")], na.rm = T)
 dat$fidelity_implementation_z <- as.numeric(scale(dat$fidelity_implementation))
 
 dat$fidelity_contextual <- rowSums(dat[,c("density_freelunch_z", "control_residual_gap_z")], na.rm = T)
